8abac62c65db3e7544cd5ec10121b6ecb78c5867
Tarun Mahajan <tarunmahajanofficial@gmail.com>
2024-08-03 14:15:21 +0530

    update

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
+    @users_count = User.where(role: 'user').count
     @clients_count = current_user.clients.count
     @groups_count = current_user.groups.count
     @campaigns_count = current_user.campaigns.count
