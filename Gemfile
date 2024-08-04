7f8deb10492b46da311fe61e893b73353ba08ba3
Tarun Mahajan <tarunmahajanofficial@gmail.com>
2024-08-04 12:02:58 +0530

    tinymce

--- Gemfile
+++ Gemfile
@@ -84,3 +84,5 @@ group :development do
   gem 'letter_opener'
   gem 'letter_opener_web'
 end
+
+gem 'tinymce-rails'
--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,8 @@ module FossaegeanInventory
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Kolkata'
+
+    config.tinymce.install = :compile
+
   end
 end
